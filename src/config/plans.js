10de52d8a136a268d37e78c2116385088fd17838
Solar Cloud <admin@solarcloud.lat>
2026-09-06 12:02:57 -0500

    chore: Update plans config with real Tebex package IDs

--- src/config/plans.js
+++ src/config/plans.js
@@ -1,66 +1,66 @@
 module.exports = {
   vanguard: {
-    name: 'Vanguard',
-    type: 'minecraft',
+    name: "Vanguard",
+    type: "minecraft",
     memory: 4096,
     cpu: 200,
     disk: 10240,
-    tebexPackageId: null
+    tebexPackageId: 7648698,
   },
   invader: {
-    name: 'Invader',
-    type: 'minecraft',
+    name: "Invader",
+    type: "minecraft",
     memory: 6144,
     cpu: 400,
     disk: 25600,
-    tebexPackageId: null
+    tebexPackageId: 7648700,
   },
   starter: {
-    name: 'Starter',
-    type: 'minecraft',
+    name: "Starter",
+    type: "minecraft",
     memory: 8192,
     cpu: 400,
     disk: 35840,
-    tebexPackageId: null
+    tebexPackageId: 7648697,
   },
   astronaut: {
-    name: 'Astronaut',
-    type: 'minecraft',
+    name: "Astronaut",
+    type: "minecraft",
     memory: 9216,
     cpu: 500,
     disk: 51200,
-    tebexPackageId: null
+    tebexPackageId: 7648701,
   },
   scientist: {
-    name: 'Scientist',
-    type: 'minecraft',
+    name: "Scientist",
+    type: "minecraft",
     memory: 16384,
     cpu: 800,
     disk: 102400,
-    tebexPackageId: null
+    tebexPackageId: 7648706,
   },
   spark: {
-    name: 'Spark',
-    type: 'bot',
+    name: "Spark",
+    type: "bot",
     memory: 1024,
     cpu: 100,
     disk: 5120,
-    tebexPackageId: null
+    tebexPackageId: 7648709,
   },
   reactor: {
-    name: 'Reactor',
-    type: 'bot',
+    name: "Reactor",
+    type: "bot",
     memory: 2048,
     cpu: 200,
     disk: 10240,
-    tebexPackageId: null
+    tebexPackageId: 7648710,
   },
   matrix: {
-    name: 'Matrix',
-    type: 'bot',
+    name: "Matrix",
+    type: "bot",
     memory: 4096,
     cpu: 400,
     disk: 20480,
-    tebexPackageId: null
-  }
+    tebexPackageId: 7648711,
+  },
 };
